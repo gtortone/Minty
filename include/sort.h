d814e737246370daa9663bf4bf96f999e2fc473e
Gennaro Tortone <gtortone@gmail.com>
2026-05-09 13:40:13 +0200

    add sorting functions

---
+++ include/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdint.h>
+
+void sortSlotsSimple(uint32_t mapfrom[], uint32_t mapto[], uint32_t maprom[], 
+                     int32_t mapdelta[], uint32_t mapsize[], uint8_t page[], 
+                     uint8_t tipo[], int numSlots);
+
+
+#endif
