5e373154903dfb13a9496884855153996f2d2e5e
YannickAros <yannick.erb@free.fr>
2026-06-05 15:09:59 +0200

    clean up remaining hack feature artefacts

--- include/memory.h
+++ include/memory.h
@@ -40,10 +40,6 @@ struct mapHole {
    bool filled;
 };
 
-struct memHack {
-   uint16_t address;
-   uint8_t value;
-};
 
 void printSlot(uint8_t idx, uint8_t page);
 void printFilledSlots(void);
@@ -53,9 +49,6 @@ void addSlot(uint32_t from, uint32_t to, uint16_t target, uint8_t page, mapType 
 bool mapSlot(uint16_t addr, uint8_t page, uint8_t *slot);
 bool mapAddress(uint16_t addr, uint8_t page, uint32_t *romaddr, mapType *type);
 void getRAMRange(uint16_t *ramfrom, uint16_t *ramto, uint8_t *ramwidth);
-void addHack(uint16_t addr, uint8_t value);
-uint8_t getHacksNum(void);
-
 void config_memory(int cfg);
 
 #endif
