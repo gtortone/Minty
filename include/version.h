7a9052e4fd2397ada2a11692195f7055676ef68f
Gennaro Tortone <gtortone@gmail.com>
2026-06-19 19:05:23 +0200

    set default version when GIT tag is not available

--- include/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 3
-#define VERSION STR(VERSION_MAJOR) "." STR(VERSION_MINOR)
-
