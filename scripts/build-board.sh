51b9239645d18eaa4e46c1f31303ee1227f91e6f
Gennaro Tortone <gtortone@gmail.com>
2026-08-07 09:24:07 +0200

    improve build-board.sh script

--- scripts/build-board.sh
+++ scripts/build-board.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+boards=("pintycard" "pirto_ii_default" "pirto_ii_duo" "pirto_ii_sd" "pirto")
+
 board=$1
+if [[ ! " ${boards[*]} " =~ " ${board} " ]]; then
+   echo "E: board ${board} not available"
+   echo "valid boards: ${boards[@]}"
+   exit
+fi
 
 cmake -B build/$board/debug -DPICO_BOARD=$board -DCMAKE_BUILD_TYPE=Debug
 make -j -C build/$board/debug
